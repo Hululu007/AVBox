2193d39e38b6cedcb33ecbb0feaf779e97f886ad
Xiaochang <3693667273@qq.com>
2026-09-19 13:38:55 +0800

    Bump version code to 8 and version name to 1.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.github.avbox.osc"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.0.7"
         multiDexEnabled = true
         ndk {
             abiFilters += setOf("arm64-v8a")
